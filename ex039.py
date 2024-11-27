2bd2ff1a45d21a54947a237e74d91c0c14b5f23e
Felipe Dutra <72906430+Fedutr@users.noreply.github.com>
2024-11-27 00:45:56 -0300

    Add files via upload

--- ex039.py
+++ ex039.py
@@ -1,13 +1,15 @@
 from datetime import date
-ano = int(input('Qual o ano que você nasceu: '))
-anoatual = date.today().year
-if anoatual - ano < 18:
+nasc = int(input('Qual o ano que você nasceu: '))
+atual = date.today().year
+idade = atual - nasc
+saldo = 18 - idade
+if idade < 18:
+    print('Quem nasceu no ano {} tem {} anos em {}'.format(nasc, idade, atual))
     print('Não precisa se alistar ao serviço militar por enquanto')
-    print('Você deve se alistar em {} ano(s)'.format(18-(anoatual-ano)))
-elif anoatual - ano == 18:
+    print('Você deve se alistar em {}'.format(atual + saldo))
+elif idade == 18:
     print('É a hora de se alistar')
-else:
+elif idade > 18:
+    print('Quem nasceu no ano {} tem {} anos em {}'.format(nasc, idade, atual))
     print('Passou do tempo do alistamento')
-    print('Já passou {} ano(s) do seu'
-          ''
-          ' alistamento obrigatório'.format((anoatual-ano)-18))
+    print('Seu alistamento obrigatório foi em {}'.format((atual + saldo)))
